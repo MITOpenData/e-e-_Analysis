e1eabde6d0b4bd4a07f243edfad275e00a86944f
abaty <abaty@mit.edu>
2017-08-11 18:09:43 +0200

    started crosscheck code area

---
+++ Crosscheck/Settings.h
@@ -0,0 +1,21 @@
+#ifndef SETTINGS
+#define SETTINGS
+
+#include <iostream>
+#include "TH1F.h"
+
+class Settings{
+  public:
+    Settings();
+
+  private:
+
+};
+
+Settings::Settings()
+{
+  std::cout << "Getting setting.." << std::endl;
+  return;
+}
+
+#endif
