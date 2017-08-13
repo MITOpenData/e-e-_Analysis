bd7724fdd9b96bf1c9823748c290900e3cc6cdee
Yen-Jie Lee <yenjie@mit.edu>
2017-08-13 23:24:17 +0200

    add particle definition

--- TwoParticleCorrelation/utilities.h
+++ TwoParticleCorrelation/utilities.h
@@ -14,3 +14,11 @@ double dphi(double phi1,double phi2)
     if (a<-PI/2) a=2*PI+a;
     return a;
 }
+
+// BELLE Particle Definition 
+//enum SIMPLEPID {BELLE_PHOTON, BELLE_ELECTRON, BELLE_PION, BELLE_MUON, BELLE_KAON, BELLE_PROTON};
+
+// ALEPH Particle Flow Classification
+enum SIMPLEPWFLAG {ALEPH_CHARGED_TRACK, ALEPH_CHARGED_LEPTONS1, ALEPH_CHARGED_LEPTONS2, ALEPH_V0, ALEPH_PHOTON, ALEPH_NEUTRAL_HADRON};
+
+
