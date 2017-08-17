8cc0496f46e85c349e5c59011ce4923624555c7f
Chris <cfmcginn@mit.edu>
2017-08-17 14:53:22 +0200

    Moved particleData and jetData class to individual headers for modular use

---
+++ DataProcessing/include/particleData.h
@@ -0,0 +1,26 @@
+#ifndef PARTICLEDATA_H
+#define PARTICLEDATA_H
+
+class particleData{
+ public:
+  static const Int_t nMaxPart = 100000;
+
+  int nParticle;
+  int EventNo;
+  int RunNo;
+  float Energy;
+  Float_t px[nMaxPart];
+  Float_t py[nMaxPart];
+  Float_t pz[nMaxPart];
+  Float_t pt[nMaxPart];
+  Float_t pmag[nMaxPart];
+  Float_t eta[nMaxPart];
+  Float_t theta[nMaxPart];
+  Float_t phi[nMaxPart];
+  Float_t mass[nMaxPart];
+  Int_t charge[nMaxPart];
+  Int_t pwflag[nMaxPart];
+  Int_t pid[nMaxPart];
+};
+
+#endif
