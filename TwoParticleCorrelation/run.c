c8863353768c6a8822361acaf566bd2eacca24e1
Yen-Jie Lee <yenjie@mit.edu>
2017-10-01 02:34:34 +0200

    current working in progress code

--- TwoParticleCorrelation/run.c
+++ TwoParticleCorrelation/run.c
@@ -4,15 +4,15 @@ void run(     TString filename = "aaa",
               Int_t isBelle      = 0,		// BELLE analysis = 1, CMS/ALEPH analysis = 0
               Int_t isThrust     = 0, 		// Thurst Axis analysis = 1, Beam Axis analysis = 0
               Int_t isTheta      = 0, 		// Use Theta angle = 1, Use Eta = 0
-	      int maxevt       = 1000000,	// Max number of events to be processed, 0 = all events
-	      int mult_low     = 38,		// Lower cut on the event multiplicity
+	      int maxevt       = 10000000,	// Max number of events to be processed, 0 = all events
+	      int mult_low     = 50,		// Lower cut on the event multiplicity
 	      int mult_high    = 100,		// Upper cut on the event multiplicity
-	      int nbin         = 16,		// Number of bins in the correlation function
+	      int nbin         = 20,		// Number of bins in the correlation function
 	      bool verbose     = 0,		// Verbose mode
 	      int num_runs     = 10,		// 
-              double ptMin     = 0.,           // min pT of the particles used for correlation function
-              double ptMax     = 4,             // max pT of the particles used for correlation function
-              double detaRange = 3.2,             // deta window of the correlation function
+              double ptMin     = 1,           // min pT of the particles used for correlation function
+              double ptMax     = 100,             // max pT of the particles used for correlation function
+              double detaRange  =3,             // deta window of the correlation function
               Float_t ptMinForN = 0.4,          // pT min for the N_{trk}^{offline} calculation (used for event classification) 
               Float_t ptMaxForN = 100,          // pT max for the N_{trk}^{offline} calculation (used for event classification)
               Float_t etaCutForN = 2.4          // eta window for the N_{trk}^{offline} calculation (used for event classification)
@@ -33,7 +33,7 @@ void run(     TString filename = "aaa",
     //filename = "cleaned_ALEPH_Data-all.aleph.root";
 
        // With new Thrust code from Austin
-    filename = "cleaned_ALEPH_Data2-v3_Aug11_2017.root";
+//    filename = "cleaned_ALEPH_Data2-v3_Aug11_2017.root";
 
 
    analysis(filename, isBelle, isThrust, isTheta,maxevt,mult_low,mult_high,nbin,verbose,num_runs,ptMin,ptMax,detaRange,ptMinForN,ptMaxForN,etaCutForN); 
