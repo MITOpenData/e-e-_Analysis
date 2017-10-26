d5ae2afab3dbbb7ee4a9e3c89afd52f65e0433d7
Anthony Badea <badea@mit.edu>
2017-10-25 22:29:18 -0400

    added cuts based on ALEPH Paper

--- ThrustDistribution/thrust_distribution.c
+++ ThrustDistribution/thrust_distribution.c
@@ -17,13 +17,18 @@
 #include "TLorentzVector.h"
 #include "TCanvas.h"
 
+
+////// Thrust Distribution  //////
+// Select events by the following criteria
+// 1. Events accepted with at least 5 good tracks  Done
+// 2. Total charged energy > 15GeV  Done
+// 3. |cos(theta)|<0.9  To Do
+// 4. missP/Energy<0.3  Done
 void thrust_distribution(TString filename = "/home/abadea/Documents/20171022/alephDataPaths_LEP2_1995to2000.root", // file used
-                         Float_t min_missP = 0.0,   // lower cut on missP
-                         Float_t max_missP = 200,   // upper cut on missP   --> currently full range of missP
+                         Float_t cut_missP = 0.3,   // upper bound on missP/energy
                          Float_t min_TTheta = 0.0, // lower cut on TTheta
                          Float_t max_TTheta = 3.5, // upper cut on TTheta --> currently full range of TTheta
-                         Float_t min_Energy = 0.0, // lower cut on Energy
-                         Float_t max_Energy = 100.0, // upper cut on Energy
+                         Float_t min_Energy = 15, // lower cut on Energy set at 15GeV   i.e. Energy>15 to take event
                          Int_t isCharged = 0, // 0 to include all particle, 1 to only look at charged particles
                          Int_t isGen = 0    // 1 to use gen level
 )
@@ -38,6 +43,7 @@ void thrust_distribution(TString filename = "/home/abadea/Documents/20171022/ale
     Float_t px[5000];
     Float_t py[5000];
     Float_t pz[5000];
+    Float_t theta[5000];
     Float_t TTheta;
     Float_t TPhi;
     Int_t pwflag[5000];
@@ -48,6 +54,7 @@ void thrust_distribution(TString filename = "/home/abadea/Documents/20171022/ale
     t1->SetBranchAddress("px",px);
     t1->SetBranchAddress("py",py);
     t1->SetBranchAddress("pz",pz);
+    t1->SetBranchAddress("theta",theta);
     t1->SetBranchAddress("TTheta", &TTheta);
     t1->SetBranchAddress("TPhi", &TPhi);
     t1->SetBranchAddress("pwflag",pwflag);
@@ -64,32 +71,42 @@ void thrust_distribution(TString filename = "/home/abadea/Documents/20171022/ale
         
         t1->GetEntry(i);
         if (i%10000==0) cout <<i<<"/"<<nevent_process<<endl;
-        //      if(Energy>1&&Energy<2){cout<<ENERGY<<endl;continue;}
+        
+        // NUMBER 2: Cut on Energy
+        if(Energy<min_Energy){continue;}
+        // Cut on TTheta
         if(TTheta<min_TTheta || TTheta>max_TTheta)continue;
-        if(missP<min_missP || missP>min_missP)continue;
+        // NUMBER 4: Cut on missP/Energy
+        if(missP/Energy>cut_missP)continue;
+        
+        
         TVector3 v1(1,0,0);
         v1.SetPhi(TPhi);   // keeping rho and theta
         v1.SetTheta(TTheta); // keeping rho and phi
         v1.SetMag(1.0);  // keeping theta and phi
-        //Float_t xx,yy,zz = v1.X(),v1.Y(),v1.Z();
+        
+        
         Float_t T_sum = 0.0;
         Float_t T_mag = 0.0;
+        Int_t pwflag0 = 0;
+        
         for (Int_t j=0;j<nParticle;j++)
         {
-            if(isCharged == 1 && pwflag[j]!=0){continue;}
+            // NUMBER 3: Cut of particle theta
+            if(fabs(cos(theta[j]))>0.9)continue;
+            //if(isCharged == 1 && pwflag[j]!=0){continue;}
+            if(pwflag[i]==0)pwflag0+=1;
             TVector3 v2(px[j],py[j],pz[j]);
             if(fabs(v1.Dot(v2))>v2.Mag())cout<<"DOT = "<<abs(v1.Dot(v2))<<" V2 Magnitude = "<<v2.Mag()<<endl;
             T_sum += fabs(v1.Dot(v2));
             T_mag += v2.Mag();
         }
-        // T.push_back(T_sum);
-        //cout<<"TSUM = "<<T_sum<<" TMAG = "<T_mag<<" T = "<<T_sum/T_mag<<endl;
-        //cout<<T_sum/T_mag<<endl;
-        h_thrust->Fill(T_sum/T_mag);
-        //cout<<T_sum/T_mag<<endl;
+        
+        // NUMBER 1: if we get 5 charged particles
+        if(pwflag0>=5)h_thrust->Fill(T_sum/T_mag);
     }
-    //cout<<"INTEGRAL"<<h_thrust->Integral()<<endl;
-    //cout<<"WIDTH"<<h_thrust->GetXaxis()->GetBinWidth(1)<<endl;
+
+    
     Double_t scale = 1.0/( h_thrust->GetXaxis()->GetBinWidth(1)*h_thrust->Integral());
     h_thrust->Scale(scale);
     
@@ -97,8 +114,6 @@ void thrust_distribution(TString filename = "/home/abadea/Documents/20171022/ale
     gStyle->SetOptStat(0);
     h_thrust->Draw();
     c2->SaveAs(Form("tDist_%.2f_%.2f_%d.pdf",min_TTheta,max_TTheta,isCharged));
-    //for(Int_t i = 0;i<h_thrust->GetNbinsX();i++)cout<<h_thrust->GetBinContent(i)<<endl;
-    // h_thrust->Print("ALL");
 }
 
 
