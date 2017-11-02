e8c78687bca5c956e9fefd4d57ee6a5578e2ba60
abaty <abaty@mit.edu>
2017-11-02 02:01:21 +0100

    added event display macro, that runs on standard NTuples

--- EventDisplay/EventDisplay.C
+++ EventDisplay/EventDisplay.C
@@ -4,45 +4,143 @@
 #include "THelix.h"
 #include "TView.h"
 #include <string>
+#include "TAttPad.h"
+#include "TMath.h"
+#include "TVector3.h"
+#include "TView3D.h"
 
-void formatHelix(THelix * h){
-  h->SetLineColor(kRed);
-  h->SetLineWidth(2);
+//nasty global variables for nextEvent() function
+std::string currentFile;
+int currentEvtIndx;
+float currentPtCut;
+
+float dR(float eta1, float phi1, float eta2, float phi2){
+  return TMath::Power(TMath::Power(TMath::ACos(TMath::Cos(phi1-phi2)),2)+TMath::Power(eta1-eta2,2),0.5);
 }
 
-void EventDisplay(std::string inputFile = "/data/abaty/ALEPHTrees/mergedLEP1_20171022.root" ,int eventIndx = 0){
+void formatHelix(THelix * h, float pz, float pt, int color = 0){
+  if(color==0) h->SetLineColor(kBlue);
+  if(color==1) h->SetLineColor(kWhite);
+  if(color==2) h->SetLineColor(kRed);
+  if(color==3) h->SetLineColor(6);
+  if(color==4) h->SetLineColor(7);
+  h->SetLineWidth(1);
+  
+  float rangeBound = 1;
+  if(pt<2.5 && TMath::Abs(pz)<0.5) rangeBound = TMath::Abs(pz);
+  h->SetRange(0,rangeBound);
+  if(pz<0) h->SetRange(-rangeBound,0);
+}
+
+
+void EventDisplay(std::string inputFile = "/data/abaty/ALEPHTrees/mergedLEP1_20171022.root" ,int eventIndx = 101137, float ptCut = 0){
+
+  currentFile=inputFile;
+  currentEvtIndx=eventIndx;
+  currentPtCut=ptCut;
 
   TFile * f = TFile::Open(inputFile.c_str(),"read");
   TTree * t = (TTree*)f->Get("t");
+  TTree * jt = (TTree*)f->Get("ak8ESchemeJetTree"); 
 
   int nParticle;
+  float pt[500];
+  float eta[500];
+  float phi[500];
   float px[500];
   float py[500];
   float pz[500];
   float charge[500];
   int pwflag[500], pwflagMix[500];
 
+  int nref;
+  float jtpt[50];
+  float jteta[50];
+  float jtphi[50];
+
+  float TTheta;
+  float TPhi;
+  t->SetBranchAddress("TTheta_charged",&TTheta);
+  t->SetBranchAddress("TPhi_charged",&TPhi);    
   t->SetBranchAddress("nParticle",&nParticle);
+  t->SetBranchAddress("pt",&pt);
+  t->SetBranchAddress("phi",&phi);
+  t->SetBranchAddress("eta",&eta);
   t->SetBranchAddress("px",&px);              
   t->SetBranchAddress("py",&py);              
   t->SetBranchAddress("pz",&pz);              
   t->SetBranchAddress("pwflag",&pwflag);     
   t->SetBranchAddress("charge",&charge); 
+  jt->SetBranchAddress("nref",&nref);
+  jt->SetBranchAddress("jtpt",&jtpt);
+  jt->SetBranchAddress("jteta",&jteta);
+  jt->SetBranchAddress("jtphi",&jtphi);
   t->GetEntry(eventIndx);
+  jt->GetEntry(eventIndx);
+  TVector3 thrust = TVector3(0,0,0);
+  thrust.SetMagThetaPhi(10,TTheta,TPhi);
 
-  TCanvas* helix_example_c1 = new TCanvas("helix_example_c1");
+
+  TCanvas* c1 = new TCanvas("AzimuthalView","AzimuthalView",600,600);
+  c1->SetFillColor(kBlack);  
   TView *view = TView::CreateView(1);
   view->SetRange(-1,-1,-1,1,1,1);
+  view->TopView();
+
+  TCanvas* c2 = new TCanvas("TopView","TopView",600,600);
+  c2->SetFillColor(kBlack);  
+  TView *view2 = TView::CreateView(1);
+  view2->SetRange(-1,-1,-1,1,1,1);
+  view2->SideView();
+  
+  TCanvas* c3 = new TCanvas("AzimuthalThrustView","AzimuthalThrustView",600,600);
+  c3->SetFillColor(kBlack);  
+  TView *view3 = TView::CreateView(1);
+  view3->SetRange(-1,-1,-1,1,1,1);
+  view3->RotateView(TPhi*180/TMath::Pi(),TTheta*180/TMath::Pi());
+  view3->ZoomView(c3,10);
 
   THelix * helix[1000];
+  helix[999] = new THelix(0,0,0,thrust.Px(),thrust.Py(),thrust.Pz(),0.000001);
+  helix[999]->SetRange(-1,1);
+  helix[999]->SetLineColor(8);
+  helix[999]->SetLineWidth(3);
+  c1->cd();
+  helix[999]->Draw();
+  c2->cd();
+  helix[999]->Draw();
+  c3->cd();
+  helix[999]->Draw();
+
   int nHelix = 0;
   for(int i = 0; i<nParticle; i++){
     if(pwflag[i]!=0) continue;
-    std::cout << i<<" "  << px[i]<<" "  << py[i]<<" " << pz[i]<<" " << charge[i] << std::endl; 
+    if(pt[i]<ptCut) continue;
+    //jet tracks
+    int trackColor = 0;
+    if(jtpt[0]>5 && dR(jteta[0],jtphi[0],eta[i],phi[i])<0.8) trackColor = 1;
+    if(jtpt[1]>5 && dR(jteta[1],jtphi[1],eta[i],phi[i])<0.8) trackColor = 2;
+    if(jtpt[2]>5 && dR(jteta[2],jtphi[2],eta[i],phi[i])<0.8) trackColor = 3;
+    if(jtpt[3]>5 && dR(jteta[3],jtphi[3],eta[i],phi[i])<0.8) trackColor = 4;
+
+    //std::cout <<  jteta[0] << " " << jtphi[0] <<" " <<  eta[i] << " " << phi[i] << std::endl;
+    //std::cout <<  dR(jteta[0],jtphi[0],eta[i],phi[i]) << std::endl;
+
+    //std::cout << i<<" "  << px[i]<<" "  << py[i]<<" " << pz[i]<<" " << charge[i] << " " << trackColor <<  std::endl; 
     helix[nHelix] = new THelix(0,0,0,px[i],py[i],pz[i],charge[i]*1.5);
-    formatHelix(helix[nHelix]);
+    formatHelix(helix[nHelix],pz[i],pt[i],trackColor);
+    c1->cd();
+    helix[nHelix]->Draw("same");
+    c2->cd();
+    helix[nHelix]->Draw("same");
+    c3->cd();
     helix[nHelix]->Draw("same");
     nHelix++;
   }
 
+}
+
+int nextEvent(){
+  EventDisplay(currentFile,currentEvtIndx+1,currentPtCut);
+  return currentEvtIndx;
 }
