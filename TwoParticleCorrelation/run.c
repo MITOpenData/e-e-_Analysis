09c2a19559e67962d0ebee1175ec5c453bdd61ab
Anthony Badea <badea@mit.edu>
2017-11-30 12:41:03 -0500

    added min 50 nParticle data file

--- TwoParticleCorrelation/run.c
+++ TwoParticleCorrelation/run.c
@@ -2,8 +2,8 @@
 
 void scan(TString filename = "/data/flowex/CMSsample/TPCNtuple_MinBias_TuneCUETP8M1_5p02TeV-pythia8-HINppWinter16DR-NoPU_75X_mcRun2_asymptotic_ppAt5TeV_forest_v2_track.root",TString savename = "TPCNtuple_MinBias_TuneCUETP8M1_5p02TeV-pythia8-HINppWinter16DR-NoPU_75X_mcRun2_asymptotic_ppAt5TeV_forest_v2_track",Int_t isGen = 0)
 {
-    Int_t mult_low[4] = {0,20,30,0};
-    Int_t mult_high[4] = {20,30,9999,9999};
+    Int_t mult_low[4] = {20,30,50,60};
+    Int_t mult_high[4] = {9999,9999,9999,9999};
     for (int i=0;i<4;i++)
     {
         // check max event above 10000000
@@ -13,16 +13,30 @@ void scan(TString filename = "/data/flowex/CMSsample/TPCNtuple_MinBias_TuneCUETP
 
 void run()
 {
+    // Minimum Bias //
+    /*
     TString pthat1_Zee = "/data/cmcginn/GeneratorsHEPMC/PYTHIA8/0p0912/pthat1_Zee/outFile_MERGED.root";
     TString pthat1_Zee_save = "pthat1_Zee";
     scan(pthat1_Zee,pthat1_Zee_save,1);
     TString pthat1_Zee_RopeWalk = "/data/cmcginn/GeneratorsHEPMC/PYTHIA8/0p0912/pthat1_Zee_RopeWalk/outFile_MERGED.root";
     TString pthat1_Zee_RopeWalk_save = "pthat1_Zee_RopeWalk";
     scan(pthat1_Zee_RopeWalk,pthat1_Zee_RopeWalk_save,1);
+    */
+    // nParticle min 50 //
+    TString pthat1_Zee = "/data/cmcginn/GeneratorsHEPMC/PYTHIA8/0p0912/pthat1_Zee_minNPart50/outFile_MERGED.root";
+    TString pthat1_Zee_save = "pthat1_Zee_minNPart50";
+    scan(pthat1_Zee,pthat1_Zee_save,1);
+    TString pthat1_Zee_RopeWalk = "/data/cmcginn/GeneratorsHEPMC/PYTHIA8/0p0912/pthat1_Zee_RopeWalk_minNPart50/outFile_MERGED.root";
+    TString pthat1_Zee_RopeWalk_save = "pthat1_Zee_RopeWalk_minNPart50";
+    scan(pthat1_Zee_RopeWalk,pthat1_Zee_RopeWalk_save,1);
+    
+    /// LEP DATA//
+    /*
     TString LEP1 = "/home/abadea/Documents/20171022/alephDataPaths_LEP1.root";
     TString LEP1_save = "LEP1";
     scan(LEP1,LEP1_save,0);
     TString LEP2 = "/home/abadea/Documents/20171022/alephDataPaths_LEP2_1995to2000.root";
     TString LEP2_save = "LEP2";
     scan(LEP2,LEP2_save,0);
+    */
 }
