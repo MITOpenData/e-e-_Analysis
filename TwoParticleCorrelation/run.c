96931149693fb52d9d667bd5086cdabdb0800889
Anthony Badea <badea@mit.edu>
2017-12-01 16:03:30 -0500

    run.c fixes

--- TwoParticleCorrelation/run.c
+++ TwoParticleCorrelation/run.c
@@ -1,9 +1,7 @@
 #include "ridge_check.c"
 
-void scan(TString filename = "/data/flowex/CMSsample/TPCNtuple_MinBias_TuneCUETP8M1_5p02TeV-pythia8-HINppWinter16DR-NoPU_75X_mcRun2_asymptotic_ppAt5TeV_forest_v2_track.root",TString savename = "TPCNtuple_MinBias_TuneCUETP8M1_5p02TeV-pythia8-HINppWinter16DR-NoPU_75X_mcRun2_asymptotic_ppAt5TeV_forest_v2_track",Int_t isGen = 0,Int_t isThrust = 0)
+void scan(TString filename,TString savename,Int_t isGen,Int_t isThrust,Int_t mult_low[], Int_t mult_high[])
 {
-    Int_t mult_low[4] = {20,30,50,60};
-    Int_t mult_high[4] = {9999,9999,9999,9999};
     for (int i=0;i<4;i++)
     {
         // check max event above 10000000
@@ -14,21 +12,24 @@ void scan(TString filename = "/data/flowex/CMSsample/TPCNtuple_MinBias_TuneCUETP
 void run()
 {
     // Minimum Bias //
-    
+    Int_t mult_low[4] = {20,30,50};
+    Int_t mult_high[4] = {9999,9999,9999};
     TString pthat1_Zee = "/data/cmcginn/GeneratorsHEPMC/PYTHIA8/0p0912/pthat1_Zee/outFile_MERGED.root";
     TString pthat1_Zee_save = "pthat1_Zee";
-    scan(pthat1_Zee,pthat1_Zee_save,1,1);
+    scan(pthat1_Zee,pthat1_Zee_save,1,1,mult_low,mult_high);
     TString pthat1_Zee_RopeWalk = "/data/cmcginn/GeneratorsHEPMC/PYTHIA8/0p0912/pthat1_Zee_RopeWalk/outFile_MERGED.root";
     TString pthat1_Zee_RopeWalk_save = "pthat1_Zee_RopeWalk";
-    scan(pthat1_Zee_RopeWalk,pthat1_Zee_RopeWalk_save,1,1);
+    scan(pthat1_Zee_RopeWalk,pthat1_Zee_RopeWalk_save,1,1,mult_low,mult_high);
     
     // nParticle min 60 //
+    Int_t mult_low_minNPart60[1] = {60};
+    Int_t mult_high_minNPart60[1] = {9999};
     TString pthat1_Zee_minNPart60 = "/data/cmcginn/GeneratorsHEPMC/PYTHIA8/0p0912/pthat1_Zee_minNPart60/outFile_MERGED.root";
     TString pthat1_Zee_minNPart60_save = "pthat1_Zee_minNPart60";
-    scan(pthat1_Zee_minNPart60,pthat1_Zee_minNPart60_save,1,1);
+    scan(pthat1_Zee_minNPart60,pthat1_Zee_minNPart60_save,1,1,mult_low_minNPart60,mult_high_minNPart60);
     TString pthat1_Zee_RopeWalk_minNPart60 = "/data/cmcginn/GeneratorsHEPMC/PYTHIA8/0p0912/pthat1_Zee_RopeWalk_minNPart60/outFile_MERGED.root";
     TString pthat1_Zee_RopeWalk_minNPart60_save = "pthat1_Zee_RopeWalk_minNPart60";
-    scan(pthat1_Zee_RopeWalk_minNPart60,pthat1_Zee_RopeWalk_minNPart60_save,1,1);
+    scan(pthat1_Zee_RopeWalk_minNPart60,pthat1_Zee_RopeWalk_minNPart60_save,1,1,mult_low_minNPart60,mult_high_minNPart60);
     
     /// LEP DATA//
     /*
