59b7d77dcb50825ed94625f510de9ad9fd9aea1e
Anthony Badea <badea@mit.edu>
2018-01-18 11:36:50 -0500

    using thrust from tree

--- TwoParticleCorrelation/include/TPCNtupleData.h
+++ TwoParticleCorrelation/include/TPCNtupleData.h
@@ -32,6 +32,11 @@ class TPCNtupleData{
     Float_t jteta[nMaxPart];
     Float_t jtphi[nMaxPart];
     
+    //thrust axis variables
+    float pt_wrtThr[nMaxPart];
+    float eta_wrtThr[nMaxPart];
+    float theta_wrtThr[nMaxPart];
+    float phi_wrtThr[nMaxPart];
     
     bool isBelle;
     int doThrust;
@@ -48,76 +53,99 @@ class TPCNtupleData{
     }
     
     // Decide if paricle j is a charged hadron    
-    bool isChargedHadron(int j){
-       if (isBelle) {
+    bool isChargedHadron(int j)
+    {
+       if (isBelle)
+       {
           // for BELLE analysis
           if (pid[j]!=BELLE_PION&&pid[j]!=BELLE_PROTON&&pid[j]!=BELLE_KAON) return 0; 
-       } else {
+       }
+        
+       else
+       {
           // for ALEPH analysis
-         if (pwflag[j]!=ALEPH_CHARGED_TRACK) return 0;
+          if (pwflag[j]!=ALEPH_CHARGED_TRACK) return 0;
        }
        
        return 1;
     }
 
     // Return Transverse Momentum
-    Float_t getPt(int j){
+    Float_t getPt(int j)
+    {
      selfCheck();
-     if (doThrust>0){
-      p.SetXYZ(px[j],py[j],pz[j]);
-      return ptFromThrust(thrust,p);
+     if (doThrust>0)
+     {
+         return pt_wrtThr[j];
+         //p.SetXYZ(px[j],py[j],pz[j]);
+         //return ptFromThrust(thrust,p);
      }
      return pt[j];
     }
     
     // Return Pseudorapidity
-    Float_t getEta(int j){
+    Float_t getEta(int j)
+    {
      selfCheck();
-     if (doThrust>0){
-      p.SetXYZ(px[j],py[j],pz[j]);
-      return etaFromThrust(thrust,p);
+     if (doThrust>0)
+     {
+         return eta_wrtThr[j];
+         //p.SetXYZ(px[j],py[j],pz[j]);
+         //return etaFromThrust(thrust,p);
      }
      return eta[j];
     }
 
     // Return phi angle
-    Float_t getPhi(int j){
+    Float_t getPhi(int j)
+    {
      selfCheck();
-     if (doThrust>0){
-      p.SetXYZ(px[j],py[j],pz[j]);
-      return phiFromThrust(thrust,p);
+     if (doThrust>0)
+     {
+         return phi_wrtThr[j];
+         //p.SetXYZ(px[j],py[j],pz[j]);
+         //return phiFromThrust(thrust,p);
      }
      return phi[j];
     }
 
     // Return theta angle
-    Float_t getTheta(int j){
+    Float_t getTheta(int j)
+    {
      selfCheck();
-     if (doThrust>0){
-      p.SetXYZ(px[j],py[j],pz[j]);
-      return thetaFromThrust(thrust,p);
+     if (doThrust>0)
+     {
+         return theta_wrtThr[j];
+         //p.SetXYZ(px[j],py[j],pz[j]);
+         //return thetaFromThrust(thrust,p);
      }
      return theta[j];
     }
 
     // 
-    void update(){
+    void update()
+    {
        memory = pt[0]*1000+eta[0];
-       if (doThrust==1) {
+       if (doThrust==1)
+       {
          thrust.SetTheta(TTheta);
          thrust.SetPhi(TPhi);
-       } else if (doThrust==2) {
+       }
+       else if (doThrust==2)
+       {
          thrust.SetTheta(2*atan(exp(-jteta[0])));
-	 thrust.SetPhi(jtphi[0]);
+         thrust.SetPhi(jtphi[0]);
        }
     }
     
     // check if the class user remember to update the Thrust axis in the analysis code when getting a new entry.
-    void selfCheck(){
+    void selfCheck()
+    {
         if (memory!=pt[0]*1000+eta[0]) std::cout <<"Bug in the code!"<<std::endl;
     }
     
-    void setTPCTreeStatus(TTree *t1){
+    void setTPCTreeStatus(TTree *t1)
+    {
       t1->SetBranchStatus("*", 0);
       t1->SetBranchStatus("nParticle", 1);
       t1->SetBranchStatus("nChargedHadrons", 1);
@@ -127,10 +155,14 @@ class TPCNtupleData{
       t1->SetBranchStatus("pid", 1);
       t1->SetBranchStatus("phi", 1);
       t1->SetBranchStatus("mass", 1);
+      t1->SetBranchStatus("pt_wrtThr",1);
+      t1->SetBranchStatus("eta_wrtThr",1);
+      t1->SetBranchStatus("theta_wrtThr",1);
+      t1->SetBranchStatus("phi_wrtThr",1);
 
-      if (!isBelle) {
+      if (!isBelle)
+      {
         t1->SetBranchStatus("pwflag", 1);
-
         t1->SetBranchStatus("px", 1);
         t1->SetBranchStatus("py", 1);
         t1->SetBranchStatus("pz", 1);
@@ -152,15 +184,19 @@ void setupTPCTree(TTree *t1, TTree *t2, TPCNtupleData &data)
     t1->SetBranchAddress("pid",data.pid);
     t1->SetBranchAddress("phi",data.phi);
     t1->SetBranchAddress("mass",data.mass);
-
+    t1->SetBranchAddress("pt_wrtThr",data.pt_wrtThr);
+    t1->SetBranchAddress("eta_wrtThr",data.eta_wrtThr);
+    t1->SetBranchAddress("theta_wrtThr",data.theta_wrtThr);
+    t1->SetBranchAddress("phi_wrtThr",data.phi_wrtThr);
+    
     t2->SetBranchAddress("jteta",data.jteta);
     t2->SetBranchAddress("jtphi",data.jtphi);
     t2->SetBranchAddress("nref",&data.nref);
     
 
-    if (!data.isBelle) {
+    if (!data.isBelle)
+    {
         t1->SetBranchAddress("pwflag",data.pwflag);
-
         t1->SetBranchAddress("px",data.px);
         t1->SetBranchAddress("py",data.py);
         t1->SetBranchAddress("pz",data.pz);
