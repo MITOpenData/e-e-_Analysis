1a5b2001edfa868e5904bb5c4c8bf04436785d18
Anthony Badea <badea@mit.edu>
2018-01-18 13:39:36 -0500

    spaces fix

--- TwoParticleCorrelation/src/ridge_parallel.sh
+++ TwoParticleCorrelation/src/ridge_parallel.sh
@@ -7,25 +7,25 @@
 #  
 #how to run ./ridge.sh inFileName outFileName fileEvents
 #!/bin/bash
-NUMFILES = 2
+NUMFILES=2
 mkdir -p tempFiles
 cd tempFiles
-i = 1
+i=1
 
 # Always round up number of events so we don’t miss events
 # To do rounding up in truncating arithmetic, simply add (denom-1) to the numerator
 # To do round-to-nearest, add (denom/2) to the numerator (halves will round up)
 # we do rounding down here because it is ok to miss a few events but techinically wrong to double count events
-let NUMEVENTS = ($fileEvents+$NUMFILES)/$NUMFILES
+let NUMEVENTS=($fileEvents+$NUMFILES)/$NUMFILES
 
-START = 1
-FINISH = $NUMEVENTS
+START=1
+FINISH=$NUMEVENTS
 # divide up the file
 while [$i -le $NUMFILES]; do
 rooteventselector —recreate -f START -l FINISH inFileName:t $i.root
-let i = i + 1
-let START = FINISH + 1
-let FINISH = $NUMEVENTS * $i
+let i=i+1
+let START=FINISH+1
+let FINISH=$NUMEVENTS*$i
 done 
 # Divide the file into 20 subfiles we can now run ridge check on them
 
