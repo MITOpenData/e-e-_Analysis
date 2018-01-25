a19467550d347e488fef60a9c0da9137439aaa8b
Chris <cfmcginn@mit.edu>
2018-01-25 23:39:50 +0100

    Added an initial exclusive clustering algo set

--- DataProcessing/include/processJets.h
+++ DataProcessing/include/processJets.h
@@ -12,14 +12,17 @@
 
 #include "include/jetData.h"
 
-void processJets(std::vector<fastjet::PseudoJet> p, fastjet::JetDefinition jDef, fastjet::JetDefinition jDefReclust, jetData *d, const double ptCut = 0.1)
+void processJets(std::vector<fastjet::PseudoJet> p, fastjet::JetDefinition jDef, fastjet::JetDefinition jDefReclust, jetData *d, const double ptCut, const double rParam, const int nExclJet)
 {
   d->nref = 0;
   if(p.size() > 0){
     fastjet::ClusterSequence cs(p, jDef);
-    std::vector<fastjet::PseudoJet> jets = fastjet::sorted_by_pt(cs.inclusive_jets());
+    std::vector<fastjet::PseudoJet> jets;
+    if(rParam > 0) jets = fastjet::sorted_by_pt(cs.inclusive_jets());
+    else jets = fastjet::sorted_by_pt(cs.exclusive_jets(nExclJet));
+
     for(unsigned int i = 0; i < jets.size(); ++i){
-      if(jets.at(i).pt() < ptCut) break; //Arbitrarily low cut on jets, removes spike at phi zero when things become ill defined
+      if(jets.at(i).pt() < ptCut && rParam > 0) break; //Arbitrarily low cut on jets, removes spike at phi zero when things become ill defined
       d->jtpt[d->nref] = jets.at(i).pt();
       d->jtphi[d->nref] = jets.at(i).phi_std();
       d->jtm[d->nref] = jets.at(i).m();
@@ -45,25 +48,31 @@ void processJets(std::vector<fastjet::PseudoJet> p, fastjet::JetDefinition jDef,
       }
       
       for(int j = 0; j < 6; ++j){
-	fastjet::ClusterSequence csSub(subJets.at(j), jDefReclust);
-
-	std::vector<fastjet::PseudoJet> constTot = fastjet::sorted_by_pt(csSub.inclusive_jets());
-	if(constTot.size() > 1){
-	  std::cout << "WARNING - RECLUSTER OF CONSTITUENTS YIELDS GREATER THAN 1 JET" << std::endl;
-	  std::cout << "Top jet: " << jets.at(i).pt() << ", " << jets.at(i).phi() << ", " << jets.at(i).eta() << std::endl;
-	  std::cout << "Const: " << std::endl;
-	  for(unsigned int k = 0; k < subJets.at(j).size(); ++k){
-	    std::cout << " " << k << "/" << subJets.at(j).size() << ": " << subJets.at(j).at(k).pt() << ", " << subJets.at(j).at(k).phi() << ", " << subJets.at(j).at(k).eta() << std::endl;
+	if(subJets.at(j).size() == 0) d->jtptFracPW[d->nref][j] = 0.;
+	else{
+	  fastjet::ClusterSequence csSub(subJets.at(j), jDefReclust);
+	  
+	  std::vector<fastjet::PseudoJet> constTot;
+	  if(rParam > 0) constTot = fastjet::sorted_by_pt(csSub.inclusive_jets());
+	  else constTot = fastjet::sorted_by_pt(csSub.exclusive_jets(1));
+	  
+	  if(constTot.size() > 1){
+	    std::cout << "WARNING - RECLUSTER OF CONSTITUENTS YIELDS GREATER THAN 1 JET" << std::endl;
+	    std::cout << "Top jet: " << jets.at(i).pt() << ", " << jets.at(i).phi() << ", " << jets.at(i).eta() << std::endl;
+	    std::cout << "Const: " << std::endl;
+	    for(unsigned int k = 0; k < subJets.at(j).size(); ++k){
+	      std::cout << " " << k << "/" << subJets.at(j).size() << ": " << subJets.at(j).at(k).pt() << ", " << subJets.at(j).at(k).phi() << ", " << subJets.at(j).at(k).eta() << std::endl;
+	    }
+	    
+	    std::cout << "Reclust: " << std::endl;
+	    for(unsigned int k = 0; k < constTot.size(); ++k){
+	      std::cout << " " << k << "/" << constTot.size() << ": " << constTot.at(k).pt() << ", " << constTot.at(k).phi() << ", " << constTot.at(k).eta() << std::endl;
+	    }
+	    
 	  }
-
-	  std::cout << "Reclust: " << std::endl;
-	  for(unsigned int k = 0; k < constTot.size(); ++k){
-	    std::cout << " " << k << "/" << constTot.size() << ": " << constTot.at(k).pt() << ", " << constTot.at(k).phi() << ", " << constTot.at(k).eta() << std::endl;
-	  }
-
+	  else if(constTot.size() == 1) d->jtptFracPW[d->nref][j] = constTot.at(0).pt()/jets.at(i).pt();
+	  else d->jtptFracPW[d->nref][j] = 0.;
 	}
-	else if(constTot.size() == 1) d->jtptFracPW[d->nref][j] = constTot.at(0).pt()/jets.at(i).pt();
-	else d->jtptFracPW[d->nref][j] = 0.;
       }
 
       ++d->nref;
