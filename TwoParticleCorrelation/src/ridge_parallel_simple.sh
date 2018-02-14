dea464a188c9f3a72e3ab530124ad3bc3cdcb895
Anthony Badea <badea@mit.edu>
2018-02-14 14:15:24 -0500

    nicer parallel

--- TwoParticleCorrelation/src/ridge_parallel_simple.sh
+++ TwoParticleCorrelation/src/ridge_parallel_simple.sh
@@ -13,15 +13,17 @@ outFileName=$2
 
 find $inFileLoc -type f -name "*.root" >inFileList.txt
 i=0
+mkdir tempLoc_$outFileName
+
 while read line
 do
-    root -b -q ridge_check_parallel.c\(\"$line\",\"out_$i\"\) &
+    root -b -q ridge_check_parallel.c\(\"$line\",\"tempLoc_$outFileName/out_$i\"\) &
     let i=i+1
 done <inFileList.txt
 wait
 
 # hadd the files together
-hadd -f $outFileName.root out_*.root
+hadd -f $outFileName.root tempLoc_$outFileName/out_*.root
 # delete the smaller files
-rm -r out_*.root
+rm -r tempLoc_$outFileName #out_*.root
 rm -r inFileList.txt
