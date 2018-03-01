1af26600eca42300a0fc66fb4217496154aec04b
Anthony Badea <badea@mit.edu>
2018-02-28 22:54:44 -0500

    check pt range

--- TwoParticleCorrelation/include/Selection.h
+++ TwoParticleCorrelation/include/Selection.h
@@ -84,8 +84,8 @@ class Selection
         // Thrust Axis
         bool doThrust = false;  bool donTrkThrust = false; // false = use beam axis for nTrk calculation
         static const Int_t nptBins_wrtThr = 1;
-        Float_t ptBinsLow_wrtThr[nptBins_wrtThr]  = {1.0,0.4};  // measured in GeV {1.0,0.4}
-        Float_t ptBinsHigh_wrtThr[nptBins_wrtThr] = {3.0,100}; // {3.0,100.0}
+        Float_t ptBinsLow_wrtThr[nptBins_wrtThr]  = {0.4};  // measured in GeV {1.0,0.4}
+        Float_t ptBinsHigh_wrtThr[nptBins_wrtThr] = {100}; // {3.0,100.0}
         static const Int_t netaBins_wrtThr = 1;
         Float_t etaBinsLow_wrtThr[netaBins_wrtThr]  = {1.8}; //{4.5,5.0}
         Float_t missPCut_wrtThr = 20;
