29013e93e47c7fc4818ecd71f6267f02a6640afa
Anthony Badea <badea@mit.edu>
2018-03-04 18:47:14 -0500

    updated plotDQC to run in parallel

--- TwoParticleCorrelation/src/plotDQC_parallel.sh
+++ TwoParticleCorrelation/src/plotDQC_parallel.sh
@@ -13,15 +13,21 @@ outFileName=$2
 
 find $inFileLoc -type f -name "*.root" >inFileList.txt
 i=0
+mkdir tempLoc_$outFileName
+
 while read line
 do
-    root -b -q plotDQC.cc\(\"$line\",\"out_$i\"\) &
+    root -b -q plotDQC.cc\(\"$line\",\"tempLoc_$outFileName/out_$i\"\) &
     let i=i+1
 done <inFileList.txt
 wait
 
-# hadd the files together
-hadd -f $outFileName.root out_*.root
-# delete the smaller files
-rm -r out_*.root
 rm -r inFileList.txt
+
+find tempLoc_$outFileName -type f -name "*.root" > outFileList.txt
+root -b -q histoHadd_TH1F.C\(\"outFileList.txt\",\"$outFileName\"\)
+rm -r tempLoc_$outFileName
+rm -r outFileList.txt
+
+
+
