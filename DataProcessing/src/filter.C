638ddd665f43e74c26116a873a146a49c1f7d1ed
Yen-Jie Lee <yenjie@mit.edu>
2018-03-30 15:31:42 +0200

    filter on trees, working in progress

---
+++ DataProcessing/src/filter.C
@@ -0,0 +1,95 @@
+#include <TChain.h>
+#include <TFile.h>
+#include <iostream>
+
+#include "include/eventData.h"
+
+//=====================================
+// filter.C
+//
+// To filter out interesting events
+//  
+// ====================================
+
+void filter(char* fname = "/data/cmcginn/StudyMultSamples/ALEPH/MC/20180323/alephMCRecoAfterCutPaths_1994.root",char *outfile="output.root")
+{
+
+string trees[33] = {
+"t",
+"akR4ESchemeJetTree",
+"akR4WTAmodpSchemeJetTree",
+"akR8ESchemeJetTree",
+"akR8WTAmodpSchemeJetTree",
+"BoostedWTAR8Evt",
+"ktN2ESchemeJetTree",
+"ktN2WTAmodpSchemeJetTree",
+"BoostedWTAktN2Evt",
+"ktN3ESchemeJetTree",
+"ktN3WTAmodpSchemeJetTree",
+"tgen",
+"tgenBefore",
+"akR4ESchemeGenJetTree",
+"akR4ESchemeGenJetBeforeTree",
+"akR4WTAmodpSchemeGenJetTree",
+"akR4WTAmodpSchemeGenJetBeforeTree",
+"akR8ESchemeGenJetTree",
+"akR8ESchemeGenJetBeforeTree",
+"akR8WTAmodpSchemeGenJetTree",
+"akR8WTAmodpSchemeGenJetBeforeTree",
+"genBoostedWTAR8Evt",
+"genBoostedWTAR8BeforeEvt",
+"ktN2ESchemeGenJetTree",
+"ktN2ESchemeGenJetBeforeTree",
+"ktN2WTAmodpSchemeGenJetTree",
+"ktN2WTAmodpSchemeGenJetBeforeTree",
+"genBoostedWTAktN2Evt",
+"genBoostedWTAktN2BeforeEvt",
+"ktN3ESchemeGenJetTree",
+"ktN3ESchemeGenJetBeforeTree",
+"ktN3WTAmodpSchemeGenJetTree",
+"ktN3WTAmodpSchemeGenJetBeforeTree"
+};
+
+
+   TTree* ch[33];
+
+   int N = 33;
+   
+   TFile *inf = new TFile(fname);
+   TFile *outf = new TFile(outfile, "RECREATE");
+   for(int i = 0; i < N; ++i){
+      ch[i] = ((TTree*) inf->Get(trees[i].data()))->CloneTree(0);
+      cout<<"Tree loaded : "<<string(trees[i]).data()<<endl;
+      cout<<"Entries : "<<((TTree*) inf->Get(trees[i].data()))->GetEntries()<<endl;
+      ch[i]->SetMaxTreeSize(4000000000);
+   }
+   
+   TTree *t = (TTree*)inf->Get("t");
+   eventData data;
+   std::vector<std::string> inList;
+   inList.push_back("nChargedHadrons");
+   data.SetStatusAndAddressRead(t,inList);
+  
+   for (int i=0;i<t->GetEntries();i++) {
+     t->GetEntry(i);
+     if (i%1000 == 0) cout <<i<<" / "<<t->GetEntries()<<endl;
+     if (data.nChargedHadrons>=30) {
+        ch[0]->Fill();
+     }
+   } 
+/*
+   TFile* file = new TFile(outfile, "RECREATE");
+   
+   for(int i = 0; i < N; ++i){
+      file->cd();
+      cout <<string(trees[i]).data()<<endl;
+      //ch[i]->Merge(file,0,"keep");
+      
+   }
+   cout <<"Good"<<endl;
+   //file->Write();
+   file->Close();
+*/
+}
+
+
