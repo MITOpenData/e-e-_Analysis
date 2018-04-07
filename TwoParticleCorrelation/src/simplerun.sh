2269ad16fbee537db46e298c8c6d1790f651a231
ginnocen <ginnocen@cern.ch>
2018-04-07 22:20:06 +0200

    adding all the scans

--- TwoParticleCorrelation/src/simplerun.sh
+++ TwoParticleCorrelation/src/simplerun.sh
@@ -5,15 +5,12 @@ DOCOPYPLOTS=0
 mix="0"
 overwrite=1
 VERBOSE=1
-ajrej=0
-threejet=0
-ajrejcut=0
-threejetcut=0
 
-listsample=(0) #0=data, 1=mc
-listetaselection=(1) #from this list of values (0 0.05 0.1 0.3) 
-listgen=(0) #0=no gen selection, 1=gen selection
+listsample=(0 1) #0=data, 1=mc
+listetaselection=(0 1 2 3 4) #from this list of values (0 0.3 0.5 1.0 2.0) 
+listgen=(0 1) #0=no gen selection, 1=gen selection
 listaxis=(1) #0=beam, 1=thrust, 2=wta, 3=thrust perp, 4 =wta perp 
+listthirdjet=(0 1) #from this list of values (0 0.05 0.1 0.3) 
 
 ################################################################
 #### dont change anything below this if you dont know what you are doing #### 
@@ -24,8 +21,14 @@ listthrust=(0 1 0 1 0)
 listtwta=(0 0 1 0 1)
 listtperp=(0 0 0 1 1)
 
-listetacuts=(0 0.05 0.1 0.3) 
-activateetacut=(0 1 1 1) 
+listetacuts=(0 0.3 0.5 1.0 2.0) 
+activateetacut=(0 1 1 1 1) 
+
+listthirdjetcut=(0 0.05 0.1 0.3) 
+activatethirdjetcut=(0 1 1 1) 
+
+ajrejcut=0.0
+ajrej=0
 
 #REGULAR ANALYSIS, CENTRAL VALUES
 AINPUT=( "/data/cmcginn/StudyMultSamples/ALEPH/LEP1/20180322/LEP1Data1992_recons_aftercut-MERGED.root" "/data/cmcginn/StudyMultSamples/ALEPH/MC/20180323/alephMCRecoAfterCutPaths_1994.root" )
@@ -56,23 +59,34 @@ if [ $DOCENTRAL -eq 1 ]; then
     
           for etacutoption in ${netaselection[@]}
             do 
-            echo "loop"
-            INPUTDATA=${AINPUT[$isample]}
-            OUTPUT=${AOUTPUT[$isample]}
-            suffix=${OUTPUT}_$(produce_postfix ${thrust} ${mix} ${wta} ${perp} ${gen} ${ajrej} ${ajrejcut} ${threejet} ${threejetcut} ${etacutoption} ${etacut})
-            sleep .5  
-            OUTPUTROOT=rootfiles/${suffix}.root
-            OUTPUTHISTO=rootfiles/2PC_${suffix}.root
-            FOLDERPLOTS=plots/plots_${suffix}
-            OUTPUTPLOTS=$FOLDERPLOTS/${suffix}
-            echo $OUTPUTROOT
-            rm $OUTPUTHISTO
-            rm -rf $FOLDERPLOTS
-            mkdir $FOLDERPLOTS 
-            rm $OUTPUTROOT
-            #root -l -q -b "ridge_check.c+(\"$INPUTDATA\",\"$OUTPUTROOT\",\"$mix\","$overwrite","$thrust","$wta","$perp","$gen","$VERBOSE","${ajrej}","${ajrejcut}","${threejet}","${threejetcut}","${etacutoption}","${etacut}")"
-            #root -l -q -b "TPCPlots.cc+(\"$OUTPUTROOT\",\"$OUTPUTHISTO\",\"$OUTPUTPLOTS\")" 
             
+            for ithird in ${listthirdjet[@]}
+            do 
+              threejetcut=${listthirdjetcut[$ithird]}
+              threejet=${activatethirdjetcut[$ithird]}
+
+              if [ $threejet -eq 1 ]; then 
+                ajrej=1
+                ajrejcut=0.1
+              fi
+        
+              echo "loop"
+              INPUTDATA=${AINPUT[$isample]}
+              OUTPUT=${AOUTPUT[$isample]}
+              suffix=${OUTPUT}_$(produce_postfix ${thrust} ${mix} ${wta} ${perp} ${gen} ${ajrej} ${ajrejcut} ${threejet} ${threejetcut} ${etacutoption} ${etacut})
+              sleep .5  
+              OUTPUTROOT=rootfiles/${suffix}.root
+              OUTPUTHISTO=rootfiles/2PC_${suffix}.root
+              FOLDERPLOTS=plots/plots_${suffix}
+              OUTPUTPLOTS=$FOLDERPLOTS/${suffix}
+              echo $OUTPUTROOT
+              rm $OUTPUTHISTO
+              rm -rf $FOLDERPLOTS
+              mkdir $FOLDERPLOTS 
+              rm $OUTPUTROOT
+              root -l -q -b "ridge_check.c+(\"$INPUTDATA\",\"$OUTPUTROOT\",\"$mix\","$overwrite","$thrust","$wta","$perp","$gen","$VERBOSE","${ajrej}","${ajrejcut}","${threejet}","${threejetcut}","${etacutoption}","${etacut}")"
+              root -l -q -b "TPCPlots.cc+(\"$OUTPUTROOT\",\"$OUTPUTHISTO\",\"$OUTPUTPLOTS\")" 
+              done # with three jets
             done # with eta cut option
           done # with eta selection
         done #done with gen 
