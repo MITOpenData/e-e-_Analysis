32d84d868127411c336bd4c31decf0ea725ce289
ginnocen <ginnocen@cern.ch>
2018-04-23 17:05:01 +0200

    adding MC

--- TwoParticleCorrelation/src/runDAQ.sh
+++ TwoParticleCorrelation/src/runDAQ.sh
@@ -1,6 +1,8 @@
 INPUTDATA="/data/cmcginn/StudyMultSamples/ALEPH/LEP1/20180402YJTest/LEP1Data1993_recons_aftercut-MERGED.root"
-OUTPUTROOT="test.root"
-INPUTDATAMIX="0"
+OUTPUTROOTData="samplevalidationDataThrust.root"
+INPUTMC="/data/cmcginn/StudyMultSamples/ALEPH/MC/20180323/alephMCRecoAfterCutPaths_1994.root"
+OUTPUTROOTMC="samplevalidationMCThrust.root"
+NPUTDATAMIX="0"
 overwrite=1
 thrust=1
 wta=0
@@ -19,5 +21,6 @@ etabarrelcutforEselection=2.0
 maxrelenergyinsidebarrel=0.2
 typemultiplicity=1
 
-root -l -q -b "DataQuality.c+(\"$INPUTDATA\",\"$OUTPUTROOT\",\"$INPUTDATAMIX\","$overwrite","$thrust","$wta","$perp","$gen","$VERBOSE","${ajrej}","${ajrejcut}","${threejet}","${threejetcut}","${owbarrel}","${anatyperegion}","${etabarrelcut}","${typeEnergyBarrelSel}","${etabarrelcutforEselection}","${maxrelenergyinsidebarrel}","${typemultiplicity}")"
+root -l -q -b "DataQuality.c+(\"$INPUTDATA\",\"$OUTPUTROOTData\",\"$INPUTDATAMIX\","$overwrite","$thrust","$wta","$perp","$gen","$VERBOSE","${ajrej}","${ajrejcut}","${threejet}","${threejetcut}","${owbarrel}","${anatyperegion}","${etabarrelcut}","${typeEnergyBarrelSel}","${etabarrelcutforEselection}","${maxrelenergyinsidebarrel}","${typemultiplicity}")"
+root -l -q -b "DataQuality.c+(\"$INPUTMC\",\"$OUTPUTROOTMC\",\"$INPUTDATAMIX\","$overwrite","$thrust","$wta","$perp","$gen","$VERBOSE","${ajrej}","${ajrejcut}","${threejet}","${threejetcut}","${owbarrel}","${anatyperegion}","${etabarrelcut}","${typeEnergyBarrelSel}","${etabarrelcutforEselection}","${maxrelenergyinsidebarrel}","${typemultiplicity}")"
 
