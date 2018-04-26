d349a7155afcd1b9d55827f36f6e47d8e2e6a9a0
Michael Peters <mjpianist.000@gmail.com>
2018-04-26 05:10:38 -0400

    Added naive efficiency table, and script for generating table (GetNaiveEff.C)

---
+++ TwoParticleCorrelation/include/GetNaiveEff.C
@@ -0,0 +1,70 @@
+#include "TFile.h"
+#include "TMath.h"
+#include "TTree.h"
+#include "TH3.h"
+
+int GetNaiveEff()
+{
+	int ptbins = 10;
+	int thetabins = 10;
+	int phibins = 10;
+	Float_t maxpt = 40;
+	int maxmult = 100;
+	TFile* f = TFile::Open("alephMCRecoAfterCutPaths_1994.root");
+	TTree* t = (TTree*)f->Get("t");
+	TTree* tgen = (TTree*)f->Get("tgen");
+	ULong64_t nevents = t->GetEntriesFast();
+	ULong64_t neventsgen = tgen->GetEntriesFast();
+	ULong64_t nevt = nevents>neventsgen?nevents:neventsgen;
+	TH3F* eff = new TH3F("eff","eff",ptbins,0,maxpt,thetabins,0,TMath::Pi(),phibins,-TMath::Pi(),TMath::Pi());
+	TH3F* effgen = new TH3F("effgen","effgen",ptbins,0,maxpt,thetabins,0,TMath::Pi(),phibins,-TMath::Pi(),TMath::Pi());
+	Float_t pt[maxmult];
+	Float_t ptgen[maxmult];
+	Float_t theta[maxmult];
+	Float_t thetagen[maxmult];
+	Float_t phi[maxmult];
+	Float_t phigen[maxmult];
+	Int_t mult;
+	Int_t multgen;
+	t->SetBranchAddress("pt",pt);
+	tgen->SetBranchAddress("pt",ptgen);
+	t->SetBranchAddress("theta",theta);
+	tgen->SetBranchAddress("theta",thetagen);
+	t->SetBranchAddress("phi",phi);
+	tgen->SetBranchAddress("phi",phigen);
+	t->SetBranchAddress("nParticle",&mult);
+	tgen->SetBranchAddress("nParticle",&multgen);
+	for(ULong64_t i=0;i<nevt;i++)
+	{
+		if(i % 1000 == 0) std::cout << i << "/" << nevt << std::endl;
+		t->GetEntry(i);
+		tgen->GetEntry(i);
+		for(int j=0;j<mult;j++) eff->Fill(pt[j],theta[j],phi[j]);
+		for(int j=0;j<multgen;j++) effgen->Fill(ptgen[j],thetagen[j],phigen[j]);
+	}
+	eff->Sumw2();
+	effgen->Sumw2();
+	eff->Divide(effgen);
+	//print out code for efficiency table
+	std::cout << "Float_t eff[ptbins][thetabins][phibins] = ";
+	std::cout << "{";
+	for(int i=0;i<ptbins;i++)
+	{
+		std::cout << "{";
+		for(int j=0;j<thetabins;j++)
+		{
+			std::cout << "{";
+			for(int k=0;k<phibins;k++)
+			{
+				std::cout << eff->GetBinContent(i+1,j+1,k+1);
+				if(k!=phibins-1) std::cout << ", ";
+			}
+			std::cout << "}";
+			if(j!=thetabins-1) std::cout << "," << std::endl;
+		}
+		std::cout << "}";
+		if(i!=ptbins-1) std::cout << "," << std::endl;
+	}
+	std::cout << "};";
+	return 0;
+}
