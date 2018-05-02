4d1092488ff60a873cc023ab3eb609314b6c134a
Chris <cfmcginn@mit.edu>
2018-05-02 18:18:21 +0200

    Added counter for terminating error messages

--- DataProcessing/include/alephTrkEfficiency.h
+++ DataProcessing/include/alephTrkEfficiency.h
@@ -16,12 +16,14 @@ class alephTrkEfficiency{
   ~alephTrkEfficiency();
 
   Float_t efficiency(Float_t theta, Float_t phi, Float_t pt);
+  Int_t counter;
 };
 
 alephTrkEfficiency::alephTrkEfficiency()
 {
   _effInf = new TFile("tables/efficiency_hist.root","READ");
   _heff = (TH3F*)_effInf->Get("eff");
+  counter = 0;
   return;
 }
 
@@ -30,8 +32,10 @@ alephTrkEfficiency::~alephTrkEfficiency(){if(_effInf != NULL) delete _effInf;}
 Float_t alephTrkEfficiency::efficiency(Float_t theta, Float_t phi, Float_t pt)
 {
   Float_t e = _heff->GetBinContent(_heff->FindBin(pt,theta,phi));
-  if (e==0) {
+  if (e==0 && counter < 10) {
     std::cout <<"!!!Error on efficiency correction! Zero efficiency!!! theta="<<theta<<" phi="<<phi<<" pt="<<pt<<std::endl<<std::endl;
+    if(counter == 9) std::cout << " !!!Greater than ten calls of this error... TERMINATING OUTPUT, PLEASE FIX" << std::endl;
+    ++counter;
     e=1;
   }  
   return e;
