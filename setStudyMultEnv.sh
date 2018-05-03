65a224519908c832726b97fc372e56f050f33892
Chris <cfmcginn@mit.edu>
2018-05-03 14:55:01 +0200

    Fixed output typo in environment setting

--- setStudyMultEnv.sh
+++ setStudyMultEnv.sh
@@ -5,7 +5,7 @@ export STUDYMULTDIR=$PWD
 
 export FASTJETDIR="/afs/cern.ch/work/c/cmcginn/public/Fastjet/fastjet-install"
 echo "SETTING NEW ENVIRONMENT VARIABLE FASTJETDIR AS '$FASTJETDIR'"
-echo "   !!!IF YOU ARE NOTE ON LXPLUS, PLEASE POINT FASTJETDIR TO YOUR OWN FASTJET INSTALL!!!"
+echo "   !!!IF YOU ARE NOT ON LXPLUS, PLEASE POINT FASTJETDIR TO YOUR OWN FASTJET INSTALL!!!"
 echo ""
 
 echo "All environment variables set"
